e1448bf1d8bfe737a85ad5c40b549ea6dadc6ce3
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-04-17 13:31:27 -0700

    Fetches books.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  const result = fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then(data => renderBooks(data))
+
+    return result;
 }
 
 function renderBooks(books) {
@@ -15,3 +19,28 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+// function json(series) {
+//   let characterCount = 0;
+//   let targetObj = {
+// 	targetUrl : null,
+// 	book : null
+//   }
+//   for (book of series) {
+//     const characters = book.characters;
+//     for (url of characters) {
+//       const charArray = url.split("/");
+//       characterCount += parseInt(charArray[charArray.length - 1]);
+//       if (characterCount >= 1031) {
+//         targetObj.targetUrl = url;
+// 		targetObj.book = book.url.split("/").length - 1;
+//       }
+//     }
+//   }
+//   return targetObj;
+// }
+
+// fetch("https://anapioficeandfire.com/api/books")
+//   .then((resp) => resp.json())
+//   .then(json)
+//   .then((result) => console.log(result));
